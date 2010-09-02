7b783c7d20cf31f2203df8e2f73168d93c2323f9
Alex Crichton <alex@alexcrichton.com>
2010-09-01 23:15:18 -0400

    Initial import

---
+++ vendor/rfuse-ng/ext/helper.h
@@ -0,0 +1,14 @@
+#include <sys/stat.h>
+#include <sys/statvfs.h>
+#include <ruby.h>
+#include <fuse.h>
+
+#ifndef _RHUSE_HELPER_H
+#define _RHUSE_HELPER_H
+
+void rstat2stat(VALUE rstat,struct stat *statbuf);
+void rstatvfs2statvfs(VALUE rstatvfs,struct statvfs *statvfsbuf);
+void rfuseconninfo2fuseconninfo(VALUE rfuseconninfo,struct fuse_conn_info *fuseconninfo);
+struct fuse_args * rarray2fuseargs(VALUE rarray);
+
+#endif
