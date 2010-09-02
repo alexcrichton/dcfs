5b1246f4f3ae05d366fa2dd596189c8e95401597
Alex Crichton <alex@alexcrichton.com>
2010-09-02 14:03:14 -0400

    Remove rfuse-ng

--- lib/dtellafs.rb
+++ lib/dtellafs.rb
@@ -1,15 +1,10 @@
 begin
   require 'fusefs'
 rescue LoadError
-  require File.expand_path('../../vendor/fusefs/ext/fusefs_lib', __FILE__)
+  $LOAD_PATH << File.expand_path('../../vendor/fusefs/ext', __FILE__)
   require File.expand_path('../../vendor/fusefs/lib/fusefs', __FILE__)
 end
 
 module DtellaFS
-  autoload :File, 'dtellafs/file'
-  autoload :Dir, 'dtellafs/dir'
-  autoload :Stat, 'dtellafs/stat'
-  autoload :StatVfs, 'dtellafs/stat_vfs'
-  autoload :Fuse, 'dtellafs/fuse'
   autoload :VERSION, 'dtellafs/version'
 end
