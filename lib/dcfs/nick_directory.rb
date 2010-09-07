c165853673ef69b8ff1dd78e17dd267497da882d
Alex Crichton <alex@alexcrichton.com>
2010-09-06 20:45:06 -0400

    Trying to get this working over DRb

--- lib/dcfs/nick_directory.rb
+++ lib/dcfs/nick_directory.rb
@@ -3,15 +3,10 @@ require 'active_support/core_ext/object/try'
 module DCFS
   class NickDirectory
 
+    attr_accessor :file_list
+
     def initialize nick, client
       @nick, @client = nick, client
-
-      # @client.subscribe do |type, message|
-      #   if type == :file_list && message[:nick] == @nick
-      #     @file_list = @client.file_list(@nick)
-      #   end
-      # end
-      # load_file_list
     end
 
     def contents path
@@ -19,15 +14,11 @@ module DCFS
       drilldown(path).try :keys
     end
 
-    def size(path)
-      400
-      # if path == '/'
-      #   442246
-      # else
-      #   read_file(path).size
-      # end
+    def size path
+      entity = drilldown path
+      entity.is_a?(Struct) ? entity.size : 4096
     end
-    
+
     def directory? path
       load_file_list if @file_list.nil?
       drilldown(path).is_a? Hash
@@ -41,17 +32,18 @@ module DCFS
     def read_file(path)
       "Hello, World!\n"
     end
-    
+
     protected
-    
+
     def drilldown path
-      path.split('/').inject(@file_list) { |hash, part| 
+      path.split('/').inject(@file_list) { |hash, part|
         hash ? hash[part] : nil
       }
     end
-    
+
     def load_file_list
-      @file_list = @client.file_list!(@nick) || {}
+      @file_list = @client.file_list!(@nick)
+      # @file_list = {}
     end
   end
 end
--- lib/dcfs/root.rb
+++ lib/dcfs/root.rb
@@ -6,6 +6,11 @@ Thread.abort_on_exception = true
 module DCFS
   class Root < FuseFS::MetaDir
 
+    def initialize
+      @nicks = {}
+      super
+    end
+
     def contents path
       subscribe if @client.nil?
 
@@ -20,48 +25,45 @@ module DCFS
         @real_client.connect
         DRb.thread.join
       }
-      DRb.start_service
     end
 
     def client
-      @client ||= DRbObject.new nil, 'drbunix:///tmp/dcfs.sock'
+      @client ||= begin
+        DRb.stop_service
+        DRb.start_service 'drbunix:///tmp/dcfs2.sock'
+        DRbObject.new nil, 'drbunix:///tmp/dcfs.sock'
+      end
     end
 
     protected
 
     def subscribe
       client.nicks.each{ |n| register_nick n }
+
       client.subscribe do |type, map|
         case type
           when :hello
             unless directory? map[:who]
               register_nick map[:who]
             end
-            # @nicks << map[:who] unless @nicks.include? map[:who]
-          # when :myinfo
-          #   @nick_info[map[:nick]] = map
           when :nick_list
             map[:nicks].each{ |n| register_nick n }
-            # @nicks = map[:nicks]
           when :quit
             unregister_nick map[:who]
-            # @nicks.delete map[:who]
-            # @nick_info.delete map[:who]
           when :hub_disconnected
-            # @nicks.clear
-
-            # @nick_info.clear
-          # when :userip
-            # map[:users].each_pair{ |nick, ip| @nick_info[nick][:ip] = ip }
+            # Clear out all directories?
+          when :file_list
+            @nicks[map[:nick]].file_list = client.file_list map[:nick]
         end
       end
     end
 
     def register_nick nick
-      mkdir '/' + nick, DCFS::NickDirectory.new(nick, @client)
+      mkdir '/' + nick, @nicks[nick] = DCFS::NickDirectory.new(nick, @client)
     end
 
     def unregister_nick nick
+      @nicks.delete nick
       rmdir nick
     end
 
